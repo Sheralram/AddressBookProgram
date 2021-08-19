a130db752b4ba1bef3fa7e807d1720742bf83369
Sheralram <ramkrishna.sheral@gmail.com>
2021-08-19 16:49:41 +0530

    Add New Contact to address Book

--- src/com/bridgelabz/AddressBookProgram.java
+++ src/com/bridgelabz/AddressBookProgram.java
@@ -21,30 +21,30 @@ public class AddressBookProgram {
     public static void getInput(){
     detailsAddressBook details= new detailsAddressBook();
     Scanner input = new Scanner(System.in);
-        System.out.println("Enter the following address details ");
+        System.out.println("Enter the following  address details as below:  ");
 
-
+        System.out.println("First Name: ");
         details.setFirstName(input.nextLine());
         System.out.println("First Name :"+details.getFirstName());
-
+        System.out.println("Last Name:");
         details.setLastName(input.nextLine());
         System.out.println("Last Name: "+details.getLastName());
-
+        System.out.println("email address");
         details.setEmailAddress(input.nextLine());
         System.out.println("email address: "+details.getEmailAddress());
-
+        System.out.println("Address");
         details.setAddress(input.nextLine());
         System.out.println("address: "+details.getAddress());
-
+        System.out.println("City:");
        details.setCity(input.nextLine());
         System.out.println("City :"+details.getCity());
-
+        System.out.println("State:");
        details.setState(input.nextLine());
         System.out.println("State:"+details.getState());
-
+        System.out.println("Zip:");
         details.setZip(input.nextLong());
         System.out.println("Zip:"+details.getZip());
-
+        System.out.println("Phone Number:");
         details.setPhoneNumber(input.nextLong());
         System.out.println("Phone Number:"+details.getPhoneNumber());
 
